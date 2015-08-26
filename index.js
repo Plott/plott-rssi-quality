fd3dfebaae161250f76915d3e8b387699ac6200a
Corey White <smortopahri@gmail.com>
2015-08-25 23:19:48 -0400

    Added isInteger method support for ES5

--- index.js
+++ index.js
@@ -10,12 +10,18 @@
  *
  * //=quality
  */
+var isInteger = Number.isInteger || function(value) {
+    return typeof value === "number" &&
+           isFinite(value) &&
+           Math.floor(value) === value;
+};
+
 var getQuality = function (dBm) {
   return parseFloat(((2 * (dBm + 100)) / 100).toFixed(2));
 }
 
 module.exports = function (dBm) {
-  if (!Number.isInteger(dBm)) throw new Error ('dBm must be an integer');
+  if (!isInteger(dBm)) throw new Error ('dBm must be an integer');
   var quality = dBm <= -100 ? 0 : dBm >= -50 ? 100 : getQuality(dBm);
   return quality;
 };
